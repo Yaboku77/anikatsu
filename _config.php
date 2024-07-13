c5977b1e0de05b34495b6937736ba1051f4a1b5d
Yaboku77 <90348922+Yaboku77@users.noreply.github.com>
2024-07-13 20:42:35 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -1,17 +1,17 @@
 <?php 
 
-$websiteTitle = "AniKatsu"; // Website Name
+$websiteTitle = "AniKaizoku"; // Website Name
 $websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
 $websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
-$contactEmail = "xyz@gmail.com"; // Contact Email
+$contactEmail = "admin.anikaizoku.com"; // Contact Email
 
-$version = "0.3";
+$version = "4.3";
 
 //Donate 
 $donate = "#";
 
 // Socials 
-$telegram = "https://t.me/#"; // telegram
+$telegram = "https://t.me/anikaizoku"; // telegram
 $discord = "https://discord.com/#"; // Discord
 $redit = "#"; // Reddit
 $twitter = "#"; // Twitter
@@ -22,7 +22,7 @@ $disqus = "https://indianime.disqus.com"; // Disqus
 
 
 // API URL
-$api = "https://wordywelcomepostscript.shashankktiwari.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+$api = "https://anikatsu-api-mu.vercel.app"; // https://github.com/shashankktiwariii/anikatsu-api
 
 
 $banner = $websiteUrl . "/files/images/banner.png";  //Banner
